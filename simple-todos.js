a2c0a051328430a2a0efd6b04cb2be8cdfa7dc5d
mkess-ivy <montier.kess@gmail.com>
2015-05-21 19:42:53 -0400

    got action

--- simple-todos.js
+++ simple-todos.js
@@ -1,20 +1,16 @@
+// simple-todos.js
+
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
     }
   });
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
