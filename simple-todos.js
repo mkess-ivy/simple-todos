4b828137ba795c628e2d2e7f31e4ae388bd53ff0
mkess-ivy <montier.kess@gmail.com>
2015-05-24 10:56:07 -0400

    added accounts ui with sign in functionality

--- simple-todos.js
+++ simple-todos.js
@@ -18,7 +18,9 @@ if (Meteor.isClient) {
 
     Tasks.insert({
       text: text,
-      createdAt: new Date() // current time
+      createdAt: new Date(), // current time
+      owner: Meteor.userId(), // _id of logged in user
+      username: Meteor.user().username // username of logged in user
     });
 
     // Clear form
@@ -38,6 +40,10 @@ if (Meteor.isClient) {
     Tasks.remove(this._id);
   }
 });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
 
 
